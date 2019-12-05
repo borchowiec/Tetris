0a72819b1f02dd7a2866cc860a978f42806cfc28
borchowiec <borchowiecpatryk@gmail.com>
2019-12-05 20:27:11 +0100

    Added right panel with information.

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ function initBoard() {
 
 }
 
+function initNextBlockPanel() {
+    let panel = document.querySelector(".next-block-panel");
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let node = document.createElement("div");
+            node.setAttribute("class", "block x" + j + " y" + i);
+            panel.append(node);
+        }
+    }
+}
+
 window.onload = function() {
     initBoard();
+    initNextBlockPanel();
 };
 
